462c964a13b89d4814d42bb374856d70586d5c86
chuneetgaba-dot <chuneetgaba@gmail.com>
2026-07-17 19:56:26 +0530

    add lab resource group

--- file1.tf
+++ file1.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "prod1-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "lab-rg"
+  location = "West Europe"
+}
